0a2e03bacddcf56ef321695ac5cd5820551a400b
HannaW1986 <131193450+HannaW1986@users.noreply.github.com>
2023-04-18 20:19:23 +0200

    Update App.java

--- TestGIT/src/main/java/org/example/App.java
+++ TestGIT/src/main/java/org/example/App.java
@@ -1,13 +1,13 @@
 package org.example;
 
 /**
- * Hello world!
+ * Hello world2!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World2!" );
     }
 }
